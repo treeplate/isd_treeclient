0fc06ba06fe63a4408e393a02ec991f007ef29ae
treeplate <tree.plate.moo@gmail.com>
2024-08-07 11:34:01 -0700

    add option to collapse orbits

--- lib/systemview.dart
+++ lib/systemview.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:isd_treeclient/ui-core.dart';
 import 'data-structure.dart';
 import 'assets.dart';
+import 'platform_specific_stub.dart'
+    if (dart.library.io) 'platform_specific_io.dart'
+    if (dart.library.js_interop) 'platform_specific_web.dart';
 
 class SystemSelector extends StatefulWidget {
   const SystemSelector({super.key, required this.data});
@@ -35,17 +38,19 @@ class _SystemSelectorState extends State<SystemSelector> {
   }
 }
 
-Widget renderFeature(Feature feature, DataStructure data) {
+Widget renderFeature(Feature feature, DataStructure data, bool collapseOrbits) {
   switch (feature) {
     case OrbitFeature():
       return OrbitFeatureWidget(
         feature: feature,
         data: data,
+        collapseOrbits: collapseOrbits,
       );
     case SolarSystemFeature():
       return SolarSystemFeatureWidget(
         feature: feature,
         data: data,
+        collapseOrbits: collapseOrbits,
       );
     case StructureFeature():
       return StructureFeatureWidget(
@@ -72,9 +77,13 @@ Widget renderFeature(Feature feature, DataStructure data) {
 
 class SolarSystemFeatureWidget extends StatelessWidget {
   const SolarSystemFeatureWidget(
-      {super.key, required this.feature, required this.data});
+      {super.key,
+      required this.feature,
+      required this.data,
+      required this.collapseOrbits});
   final SolarSystemFeature feature;
   final DataStructure data;
+  final bool collapseOrbits;
 
   @override
   Widget build(BuildContext context) {
@@ -84,13 +93,21 @@ class SolarSystemFeatureWidget extends StatelessWidget {
         Text('Center'),
         Padding(
           padding: EdgeInsets.only(left: 16),
-          child: AssetWidget(asset: feature.primaryChild, data: data),
+          child: AssetWidget(
+            asset: feature.primaryChild,
+            data: data,
+            collapseOrbits: collapseOrbits,
+          ),
         ),
         Text('Around'),
         ...feature.children.map(
           (e) => Padding(
             padding: EdgeInsets.only(left: 16),
-            child: AssetWidget(asset: e.child, data: data),
+            child: AssetWidget(
+              asset: e.child,
+              data: data,
+              collapseOrbits: collapseOrbits,
+            ),
           ),
         ),
       ],
@@ -103,10 +120,12 @@ class OrbitFeatureWidget extends StatelessWidget {
     super.key,
     required this.feature,
     required this.data,
+    required this.collapseOrbits,
   });
 
   final OrbitFeature feature;
   final DataStructure data;
+  final bool collapseOrbits;
 
   @override
   Widget build(BuildContext context) {
@@ -116,13 +135,21 @@ class OrbitFeatureWidget extends StatelessWidget {
         Text('Center'),
         Padding(
           padding: EdgeInsets.only(left: 16),
-          child: AssetWidget(asset: feature.primaryChild, data: data),
+          child: AssetWidget(
+            asset: feature.primaryChild,
+            data: data,
+            collapseOrbits: collapseOrbits,
+          ),
         ),
         Text('Orbiting'),
         ...feature.orbitingChildren.map(
           (e) => Padding(
             padding: EdgeInsets.only(left: 16),
-            child: AssetWidget(asset: e.child, data: data),
+            child: AssetWidget(
+              asset: e.child,
+              data: data,
+              collapseOrbits: collapseOrbits,
+            ),
           ),
         ),
       ],
@@ -145,8 +172,10 @@ class StructureFeatureWidget extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('Components (total: ${feature.hp}/${feature.minHP ?? '???'}/${feature.maxHP ?? '???'})'),
-        ...feature.materials.map((e) => Text('  ${e.material.id.toRadixString(16)} ${e.componentName == null ? '' : '${e.componentName} '}(${e.materialDescription}) ${e.quantity}/${e.requiredQuantity}'))
+        Text(
+            'Components (total: ${feature.hp}/${feature.minHP ?? '???'}/${feature.maxHP ?? '???'})'),
+        ...feature.materials.map((e) => Text(
+            '  ${e.material.id.toRadixString(16)} ${e.componentName == null ? '' : '${e.componentName} '}(${e.materialDescription}) ${e.quantity}/${e.requiredQuantity}'))
       ],
     );
   }
@@ -226,101 +255,171 @@ class SpaceSensorStatusFeatureWidget extends StatelessWidget {
 }
 
 class AssetWidget extends StatelessWidget {
-  const AssetWidget({super.key, required this.asset, required this.data});
+  const AssetWidget({
+    super.key,
+    required this.asset,
+    required this.data,
+    required this.collapseOrbits,
+  });
 
   final AssetID asset;
   final DataStructure data;
+  final bool collapseOrbits;
 
   @override
   Widget build(BuildContext context) {
     Asset asset = data.assets[this.asset]!;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TextButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return Dialog(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        asset.name ?? asset.className,
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      if (asset.name != null)
+    if (collapseOrbits &&
+        asset.features.length == 1 &&
+        asset.features.single is OrbitFeature) {
+      OrbitFeature orbit = asset.features.single as OrbitFeature;
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AssetWidget(
+            asset: orbit.primaryChild,
+            data: data,
+            collapseOrbits: collapseOrbits,
+          ),
+          if (orbit.orbitingChildren.isNotEmpty) ...[
+            Text(
+              '    Orbiting',
+            ),
+            ...orbit.orbitingChildren.map(
+              (e) => Padding(
+                padding: EdgeInsets.only(left: 16),
+                child: AssetWidget(
+                    asset: e.child, data: data, collapseOrbits: collapseOrbits),
+              ),
+            ),
+          ]
+        ],
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
                         Text(
-                          asset.className,
-                          style: TextStyle(fontSize: 10),
+                          asset.name ?? asset.className,
+                          style: TextStyle(fontSize: 20),
                         ),
-                      Text(asset.description),
-                      Text(
-                          'Owner: ${asset.owner == 0 ? 'nobody' : asset.owner == data.dynastyID ? 'you' : asset.owner}'),
-                      SelectableText('Mass: ${asset.mass} kilograms'),
-                      SelectableText('Diameter: ${asset.size} meters'),
-                    ],
-                  ),
-                );
-              },
-            );
-          },
-          child: Container(
-            decoration: BoxDecoration(border: asset.owner == data.dynastyID ? Border.all(color: Theme.of(context).colorScheme.primary, width: 5) : null,),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ColoredBox(
-                  color: Colors.grey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(2),
-                    child: Image.asset(
-                      'icons/${asset.icon}.png',
-                      width: 32,
-                      height: 32,
-                      fit: BoxFit.cover,
-                      filterQuality: FilterQuality.none,
+                        if (asset.name != null)
+                          Text(
+                            asset.className,
+                            style: TextStyle(fontSize: 10),
+                          ),
+                        Text(asset.description),
+                        Text(
+                            'Owner: ${asset.owner == 0 ? 'nobody' : asset.owner == data.dynastyID ? 'you' : asset.owner}'),
+                        SelectableText('Mass: ${asset.mass} kilograms'),
+                        SelectableText('Diameter: ${asset.size} meters'),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: asset.owner == data.dynastyID
+                    ? Border.all(
+                        color: Theme.of(context).colorScheme.primary, width: 5)
+                    : null,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ColoredBox(
+                    color: Colors.grey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(2),
+                      child: Image.asset(
+                        'icons/${asset.icon}.png',
+                        width: 32,
+                        height: 32,
+                        fit: BoxFit.cover,
+                        filterQuality: FilterQuality.none,
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  '${asset.name ?? asset.className}',
-                  style: asset.owner == 0
-                      ? DefaultTextStyle.of(context).style
-                      : TextStyle(
-                          color: getColorForDynastyID(asset.owner),
-                        ),
-                )
-              ],
+                  Text(
+                    '${asset.name ?? asset.className}',
+                    style: asset.owner == 0
+                        ? DefaultTextStyle.of(context).style
+                        : TextStyle(
+                            color: getColorForDynastyID(asset.owner),
+                          ),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        ...asset.features.map(
-          (e) => Padding(
-            padding: EdgeInsets.only(left: 16),
-            child: renderFeature(e, data),
-          ),
-        )
-      ],
-    );
+          ...asset.features.map(
+            (e) => Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: renderFeature(e, data, collapseOrbits),
+            ),
+          )
+        ],
+      );
+    }
   }
 }
 
-class SystemView extends StatelessWidget {
+class SystemView extends StatefulWidget {
   const SystemView({super.key, required this.data, required this.system});
   final DataStructure data;
   final StarIdentifier system;
 
   @override
+  State<SystemView> createState() => _SystemViewState();
+}
+
+class _SystemViewState extends State<SystemView> {
+  bool collapseOrbits = true;
+
+  @override
+  void initState() {
+    () async {
+      collapseOrbits = await getCookie('collapseOrbits') == true.toString();
+      setState(() {});
+    }();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Collapse orbits'),
+            Checkbox(
+              value: collapseOrbits,
+              onChanged: (v) {
+                setState(() {
+                  collapseOrbits = v!;
+                  setCookie('collapseOrbits', v.toString());
+                });
+              },
+            )
+          ],
+        ),
         Padding(
           padding: const EdgeInsets.only(bottom: 16),
           child: Center(
             child: SelectableText(
-              '${system.displayName} (${data.rootAssets[system]!.server})',
+              '${widget.system.displayName} (${widget.data.rootAssets[widget.system]!.server})',
               style: TextStyle(fontSize: 20),
             ),
           ),
@@ -329,11 +428,15 @@ class SystemView extends StatelessWidget {
           padding: const EdgeInsets.only(bottom: 8),
           child: Center(
             child: SelectableText(
-              'position: (${data.systemPositions[system]!.dx}, ${data.systemPositions[system]!.dy})',
+              'position: (${widget.data.systemPositions[widget.system]!.dx}, ${widget.data.systemPositions[widget.system]!.dy})',
             ),
           ),
         ),
-        AssetWidget(data: data, asset: data.rootAssets[system]!),
+        AssetWidget(
+          data: widget.data,
+          asset: widget.data.rootAssets[widget.system]!,
+          collapseOrbits: collapseOrbits,
+        ),
       ],
     );
   }
