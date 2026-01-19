4abdb7be7add00019444187373aec45c06b114ed
treeplate <tree.plate.moo@gmail.com>
2026-01-19 10:03:57 -0800

    more reasonable names + fix bug introduced in last commit

--- lib/platform_specific.dart
+++ lib/platform_specific.dart
@@ -1,4 +1,4 @@
 
-export 'platform_specific/platform_specific_stub.dart'
-    if (dart.library.io) 'platform_specific_io.dart'
-    if (dart.library.js_interop) 'platform_specific_web.dart';
+export 'platform_specific/stub.dart'
+    if (dart.library.io) 'platform_specific/io.dart'
+    if (dart.library.js_interop) 'platform_specific/web.dart';
